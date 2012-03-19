37c2ccf14aca9c7f755a2a217fd449a10c7227c9
tkrammen <tkrammen@gmail.com>
2012-03-18 23:43:14 -0300

    Update SRDeck.java

--- SRDeck.java
+++ SRDeck.java
@@ -1,5 +1,59 @@
+import java.util.*;
 
 public class SRDeck {
-	public SRDeck(){
+
+	public SRCard[] drawPile;
+	public SRCard[] discardPile;
+	
+	public ArrayList<SRCard> deck;
+	
+	public SRDeck() {
+		deck = new ArrayList<SRCard>(45);
+		for(int i = 0; i<4; i++) {
+			deck.add(new SRCard(1));
+		}for(int i = 0; i<3; i++) {
+			deck.add(new SRCard(2));
+		}for(int i = 0; i<3; i++) {
+			deck.add(new SRCard(3));
+		}for(int i = 0; i<3; i++) {
+			deck.add(new SRCard(4));
+		}for(int i = 0; i<3; i++) {
+			deck.add(new SRCard(5));
+		}for(int i = 0; i<3; i++) {
+			deck.add(new SRCard(7));
+		}for(int i = 0; i<3; i++) {
+			deck.add(new SRCard(8));
+		}for(int i = 0; i<3; i++) {
+			deck.add(new SRCard(10));
+		}for(int i = 0; i<3; i++) {
+			deck.add(new SRCard(11));
+		}for(int i = 0; i<3; i++) {
+			deck.add(new SRCard(12));
+		}for(int i = 0; i<3; i++) {
+			deck.add(new SRCard(13));
+		}
+	}
+	
+	public void shuffle() {
+		Random rand = new Random();
+		for (int i = 0; i < deck.size(); i++) {
+			int j = rand.nextInt(deck.size());
+			SRCard temp = deck.get(i);
+			deck.set(i, deck.get(j));
+			deck.set(j, temp);			
+		}
+	}		
+	
+	public SRCard drawCard() {
+		SRCard drawCard = deck.get(0);
+		deck.remove(0);
+		return drawCard;		
+	}
+	
+	public boolean isEmpty() {
+		if (deck.size() == 0) {
+		return true;
+		}
+		else return false;
 	}
 }
