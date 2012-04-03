016a6f490b5d2c3d157f57ef98056585867da3de
tkrammen <tkrammen@gmail.com>
2012-04-03 04:02:29 -0300

    Made a ton of changes, comments and test functions. Made card and rule how I thought you said you wanted them.

--- SRRule.java
+++ SRRule.java
@@ -1,51 +1,71 @@
-/*-----------------------------------------
-Programmer: Taylor Krammen
-Class: CS 205 Software Engineering
-Project: Sorry! Boardgame Implementation
--------------------------------------------*/
+import java.util.Arrays;
 
-/*
-This class will implement rules and actions for 
-each of the cards to be used in the Sorry! game.
+/**
+* This class will implement rules and actions for each of the cards to be used in the Sorry! game.
+* It allows for instructions to be passed in a modular way.
+* 
+* @author Sam Brown, Caleb Cousins, Taylor Krammen, and Yucan Zhang
 */
 
 public class SRRule {
 	
+	//Number of rules to be associated with given rule
 	public int numMoves; //Initialize numMoves
 	
-	//Will create and return the number of moves and the type of
-	//rule associated with each card.
-	
-	public String[] types(int cardNum,String type) {
-		numMoves = 0;
-		if (type == "move") {
-			numMoves = cardNum;
-			String[] rule = {""+numMoves,"move"};
+	/**
+	 * This function will create rules whose type will refer to the different 
+	 * kinds of events that will occur in the game.
+	 * 
+	 * @param cardNum
+	 * @param type
+	 * @return 
+	 */
+	 public String[] types(int cardNum) {
+		numMoves = 0; //Initialize to 0
+		if (cardNum == 1) {
+			numMoves = cardNum; //The number of moves will correlate to the card number
+			String[] rule = {""+numMoves,"move"}; //Create a string array with the number of moves allowed, and the type
 			return rule;
-		}else if (type == "start") {
+		}else if (cardNum == 2) {
 			numMoves = cardNum;
-			String[] rule = {""+numMoves,"start"};
+			String[] rule = {""+numMoves,"start, draw again"};
 			return rule;
-		}else if (type == "split") {
+		}else if (cardNum == 4) {
+			numMoves = cardNum;
+			String[] rule = {"-"+numMoves,"move backwards"};
+			return rule;
+		}else if (cardNum == 7) {
 			numMoves = cardNum;
 			String[] rule = {""+numMoves,"split"};
 			return rule;
-		}else if (type == "swap") {
+		}else if (cardNum == 10) {
+			numMoves = cardNum;
+			String[] rule = {""+numMoves,"move"};
+			return rule;
+		}else if (cardNum == 11) {
 			numMoves = cardNum;
 			String[] rule = {""+numMoves,"swap"};
 			return rule;
-		}else if (type == "drawAgain") {
-			numMoves = cardNum;
-			String[] rule = {""+numMoves,"drawAgain"};
-			return rule;
-		}else if (type == "sorry") {
+		}else if (cardNum == 0) {
 			numMoves = cardNum;
 			String[] rule = {""+numMoves,"sorry"};
 			return rule;
 		}else {
-			String[] rule = {"",""};
+			numMoves = cardNum;
+			String[] rule = {""+numMoves,"move"};
 			return rule;
 		}
 	}
-
+	/*
+	//Test function
+	public static void main(String[] args){
+		SRRule rule = new SRRule();
+		//Simple loop to make sure each of the cards is covered
+		for(int i=0;i<13;i++){
+			rule.numMoves = i;
+			System.out.println(Arrays.toString(rule.types(rule.numMoves))); //Tests SRRule's types function
+		}
+	}
+	*/
+	
 }
